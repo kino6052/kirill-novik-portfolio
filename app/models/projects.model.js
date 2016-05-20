28c1a66445dddd6ee46f8e502f8d05e0030c2d6d
Kirill Novik <jimmynovik@gmail.com>
2016-05-20 16:25:17 +0000

    Added Category Labels to Projects

--- app/models/projects.model.js
+++ app/models/projects.model.js
@@ -3,12 +3,14 @@ myApp.service('projects', function(){
         {
             name:           'Overlay Animation',
             categories:       ['css', 'css3'],
-            description:    'This Feature Helps Uses CSS3 to Create the Overlay Effect'
+            description:    'This Feature Helps Uses CSS3 to Create the Overlay Effect',
+            image: 'https://your-project-name-kino6052.c9users.io/app/common/images/sensei.png'
         },
         {
             name:           'https Server with Node.js',
             categories:       ['ssl', 'https', 'security'],
-            description:    'This Feature Helps Uses CSS3 to Create the Overlay Effect'
+            description:    'This Feature Helps Uses CSS3 to Create the Overlay Effect',
+            image: 'https://your-project-name-kino6052.c9users.io/app/common/images/ssl.png'
         }
     ];
     this.getProjectList = function(){
