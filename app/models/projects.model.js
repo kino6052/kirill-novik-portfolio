ad64eb504b7a43cd188fc788c7f1e5a70c68221a
Kirill Novik <jimmynovik@gmail.com>
2016-05-20 16:50:15 +0000

    Added Some Detail

--- app/models/projects.model.js
+++ app/models/projects.model.js
@@ -2,15 +2,31 @@ myApp.service('projects', function(){
     var projectList = [
         {
             name:           'Overlay Animation',
-            categories:       ['css', 'css3'],
+            categories:     ['css', 'css3'],
             description:    'This Feature Helps Uses CSS3 to Create the Overlay Effect',
-            image: 'https://your-project-name-kino6052.c9users.io/app/common/images/sensei.png'
+            image:          'https://your-project-name-kino6052.c9users.io/app/common/images/sensei.png',
+            done:           false
         },
         {
             name:           'https Server with Node.js',
-            categories:       ['ssl', 'https', 'security'],
+            categories:     ['ssl', 'https', 'security'],
             description:    'This Feature Helps Uses CSS3 to Create the Overlay Effect',
-            image: 'https://your-project-name-kino6052.c9users.io/app/common/images/ssl.png'
+            image:          'https://your-project-name-kino6052.c9users.io/app/common/images/ssl.png',
+            done:           false
+        },
+        {
+            name:           'Classy Gulp',
+            categories:     ['gulp'],
+            description:    'Powerful gulp script to reduce stress for developrs! Based on gulp script by @username',
+            image:          'https://your-project-name-kino6052.c9users.io/app/common/images/ssl.png',
+            done:           false
+        },
+        {
+            name:           'Powerful Grunt',
+            categories:     ['grunt'],
+            description:    'Powerful gulp script to reduce stress for developrs! Based on gulp script by @username',
+            image:          'https://your-project-name-kino6052.c9users.io/app/common/images/ssl.png',
+            done:           false
         }
     ];
     this.getProjectList = function(){
